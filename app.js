75f944abb76bad630431bb67da25acf9b69d540e
kumar Ashish <ashish@kumars-MacBook-Air.local>
2025-06-24 11:43:15 +0530

    update the CSS and HTML for better UI and made changes to JS to add search feature and inital random image load

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
 'use strict';
 
-const searchText = document.querySelector("#search-input");
+const searchtext = document.querySelector("#search-input");
+console.log(searchtext);
+
 const searchBtn = document.querySelector("#generate-btn");
-const imgContainer = document.querySelector("#image-container");
+console.log(searchBtn);
+
+const imgSection = document.querySelector("#image-container");
+console.log(imgSection);
+
 const baseURL = "https://api.unsplash.com/";
 
 
-// fucntion to fetch the Image using API and append that to the HTML
-const searchImage = async()=>{
-    imgContainer.innerHTML = '';
-    const searchString= searchText.value;
-    console.log(searchString);
-    const imageData = await fetch(`https://api.unsplash.com/search/photos?query=${searchText.value.trim()}&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`)
-    console.log(imageData);
-    const response = await imageData.json();
-    console.log(response);
-    //console.log(response.results[0].urls.full);
-    for (let pics of response.results){
-           console.log(pics.urls.thumb);
-           const img = document.createElement("img");
-           img.src = pics.urls.thumb; // or .regular
-          //img.alt = photo.alt_description || "Image"
-          imgContainer.append(img);
+searchBtn.addEventListener("click", async()=>{
+    const searchTextValue = searchtext.value.trim();
+    console.log(searchTextValue);
+
+    searchtext.value = "";
+
+    const imageSearchData = await fetch(`https://api.unsplash.com/search/photos?query=${searchTextValue}&per_page=12&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`)
+ 
+    const imageSearchResponse = await imageSearchData.json();
+    console.log(imageSearchResponse);
+
+    imgSection.innerHTML = "";
+
+    for (let imageData of imageSearchResponse.results){
+        console.log(imageData);
+        console.log(imageData.urls.regular);
+        const imgCard = document.createElement("div");
+        const imgEle = document.createElement("img");
+        imgSection.append(imgCard );
+        imgCard.append(imgEle);
+        imgEle.src = imageData.urls.regular;
     }
-   
+
+})
 
 
-}
 
-// Event listner for SearchButton
-searchBtn.addEventListener("click",searchImage);
+document.addEventListener("DOMContentLoaded", async()=>{
+     const randomImagedata = await fetch(`${baseURL}/photos/random?count=12&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
+     console.log(randomImagedata);
+
+     const randomImageResponse = await randomImagedata.json();
+     console.log(randomImageResponse);
+
+     for (let imageData of randomImageResponse){
+        console.log(imageData);
+        console.log(imageData.urls.regular);
+        const imgCard = document.createElement("div");
+        const imgEle = document.createElement("img");
+        imgSection.append(imgCard );
+        imgCard.append(imgEle);
+        imgEle.src = imageData.urls.regular;
+     }
+
+
+})
 
 
