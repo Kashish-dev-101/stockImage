048029cb00b261b7ff9dc44771be02f5318e0879
kumar Ashish <ashish@kumars-MacBook-Air.local>
2025-06-24 21:50:19 +0530

    added a Category Navigation Bar

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ console.log(searchBtn);
 const imgSection = document.querySelector("#image-container");
 console.log(imgSection);
 
+
+
 const baseURL = "https://api.unsplash.com/";
 
 
@@ -18,7 +20,8 @@ searchBtn.addEventListener("click", async()=>{
 
     searchtext.value = "";
 
-    const imageSearchData = await fetch(`https://api.unsplash.com/search/photos?query=${searchTextValue}&per_page=12&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`)
+    const imageSearchData = await fetch(`https://api.unsplash.com/search/photos?query=${searchTextValue}&per_page=20&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`)
+
  
     const imageSearchResponse = await imageSearchData.json();
     console.log(imageSearchResponse);
@@ -71,7 +74,7 @@ searchBtn.addEventListener("click", async()=>{
 
 
 document.addEventListener("DOMContentLoaded", async()=>{
-     const randomImagedata = await fetch(`${baseURL}/photos/random?count=12&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
+     const randomImagedata = await fetch(`${baseURL}/photos/random?count=50&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
      console.log(randomImagedata);
 
      const randomImageResponse = await randomImagedata.json();
@@ -80,14 +83,109 @@ document.addEventListener("DOMContentLoaded", async()=>{
      for (let imageData of randomImageResponse){
         console.log(imageData);
         console.log(imageData.urls.regular);
+
+
         const imgCard = document.createElement("div");
+        imgCard.classList.add("image-card");
+
         const imgEle = document.createElement("img");
-        imgSection.append(imgCard );
-        imgCard.append(imgEle);
         imgEle.src = imageData.urls.regular;
+
+
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
+
+        // photographer 
+
+        const photographer = document.createElement("div");
+        photographer.classList.add("photographer");
+
+
+        // Download Button 
+
+        const downloadBtn = document.createElement("button");
+        downloadBtn.classList.add("download-btn");
+        downloadBtn.textContent = "⬇️";
+
+        downloadBtn.addEventListener("click", async()=>{
+            //await fetch(`${photo.links.download_location}?client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
+            window.open(imageData.links.download, "_blank");  
+        });
+        
+        overlay.append(photographer);
+        overlay.append(downloadBtn);
+
+        imgCard.append(overlay);
+        imgCard.append(imgEle);
+        imgSection.append(imgCard );
+        
+       
+        
      }
 
 
 })
+
+
+const categoryBar = document.querySelectorAll(".category");
+console.log(categoryBar);
+
+for (let category of categoryBar){
+    console.log(category);
+    category.addEventListener("click", async()=>{
+    
+    const categoryText = category.innerText.trim();
+      
+    const categoryImageData = await fetch(`https://api.unsplash.com/search/photos?query=${categoryText}&per_page=20&client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
+
+    const imageCategoryResponse = await categoryImageData.json();
+    console.log(imageCategoryResponse);
+
+    imgSection.innerHTML = "";
+
+    for (let photo of imageCategoryResponse.results){
+        console.log(photo);
+        console.log(photo.urls.regular);
+
+        const imgCard = document.createElement("div");
+        imgCard.classList.add("image-card");
+
+        const imgEle = document.createElement("img");
+        imgEle.src = photo.urls.regular;
+        imgEle.alt = photo.alt_description || "Unsplash image";
+
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
+
+        // photographer 
+
+        const photographer = document.createElement("div");
+        photographer.classList.add("photographer");
+
+
+        // Download Button 
+
+        const downloadBtn = document.createElement("button");
+        downloadBtn.classList.add("download-btn");
+        downloadBtn.textContent = "⬇️";
+
+        downloadBtn.addEventListener("click", async()=>{
+            //await fetch(`${photo.links.download_location}?client_id=qEJS3_uc9Y5N_fd5ia0YQxM3hkwg7PrjfSphgZ3aJvo`);
+            window.open(photo.links.download, "_blank");  
+        });
+
+        overlay.append(photographer);
+        overlay.append(downloadBtn);
+
+        imgCard.append(overlay);
+        imgCard.append(imgEle);
+        imgSection.append(imgCard );
+        
+        
+    }
+
+    })
+}
+
 
 
